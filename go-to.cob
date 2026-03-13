fd6d9f241193040de8cb362aca2c32de0c1f61f6
adrivng <buddy778lol@gmail.com>
2026-03-12 23:32:53 -0500

    go-to

--- go-to.cob
+++ go-to.cob
@@ -5,13 +5,33 @@
       * Tectonics: cobc
       ******************************************************************
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       PROGRAM-ID. GO-TO.
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+       01 SI-O-NO PIC X.
        PROCEDURE DIVISION.
-       MAIN-PROCEDURE.
-            DISPLAY "Hello world"
-            STOP RUN.
-       END PROGRAM YOUR-PROGRAM-NAME.
 
+           PREGUNTA.
+               PERFORM CONTINUACION.
+
+           IF SI-O-NO = "N" OR SI-O-NO = "n"
+               GO TO FINALIZAR-PROGRAMA.
+           IF SI-O-NO = "S" OR SI-O-NO = "s"
+           ELSE
+               DISPLAY "porfavor elija uno valido".
+               PERFORM PROGRAMA.
+
+
+           FINALIZAR-PROGRAMA.
+               STOP RUN.
+
+           CONTINUACION.
+               DISPLAY "¿Desea ejecutar el programa (s/n)?".
+               ACCEPT SI-O-NO.
+
+           PROGRAMA.
+               DISPLAY "Se ejecuta programa".
+
+
+       END PROGRAM GO-TO.
